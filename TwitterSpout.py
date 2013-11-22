d4a0b02f5374b0c6b52fabda78a0e894118f9cd1
James Schlitt <jslongboards@gmail.com>
2013-11-22 17:30:19 -0500

    tweak

--- TwitterSpout.py
+++ TwitterSpout.py
@@ -8,18 +8,19 @@ def getConfig(directory):
     fileIn = open(directory)
     content = fileIn.readlines()
     for item in content:
-        while '  ' in item:
-            item = item.replace('  ',' ')
-        while '\n' in item:
-            item = item.replace('\n','')
-        line = item.split(' = ')
-        try:
-            line[1] = float(line[1])
-            if line[1] == int(line[1]):
-                line[1] = int(line[1])
-        except:
-            None
-        params[line[0]] = line[1]
+        if ' = ' in item:
+            while '  ' in item:
+                item = item.replace('  ',' ')
+            while '\n' in item:
+                item = item.replace('\n','')
+            line = item.split(' = ')
+            try:
+                line[1] = float(line[1])
+                if line[1] == int(line[1]):
+                    line[1] = int(line[1])
+            except:
+                None
+            params[line[0]] = line[1]
     print "loaded params:"
     for key,item in params:
         print '\t*', item,':', key
