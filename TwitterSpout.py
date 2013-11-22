5b673f07c7ac1ed0242d52c057da5bb4337b90c6
James Schlitt <jslongboards@gmail.com>
2013-11-22 16:46:20 -0500

    Added config loader

---
+++ TwitterSpout.py
@@ -0,0 +1,28 @@
+import sys, os
+import json
+
+def getConfig(directory):
+    params = {}
+    if directory == "null":
+        directory = ''
+    fileIn = open(directory)
+    content = fileIn.readlines()
+    for item in content:
+        while '  ' in item:
+            item = item.replace('  ',' ')
+        while '\n' in item:
+            item = item.replace('\n','')
+        line = item.split(' = ')
+        try:
+            line[1] = float(line[1])
+            if line[1] == int(line[1]):
+                line[1] = int(line[1])
+        except:
+            None
+        params[line[0]] = line[1]
+    print "loaded params:"
+    for key,item in params:
+        print '\t*', item,':', key
+    return params
+    
+getConfig('')
