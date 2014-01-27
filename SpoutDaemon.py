e6b32cf1e59b71b52638c96a64de51f902161281
James Schlitt <jslongboards@gmail.com>
2014-01-27 16:46:49 -0500

    Finished daemon for collection automation

--- SpoutDaemon.py
+++ SpoutDaemon.py
@@ -1,5 +1,5 @@
-import sys
 import subprocess
+import time
 
 try:
     fileIn = open(sys.argv[1])
@@ -19,15 +19,23 @@ for line in content:
         
 print "GDI URLS:", urls
 
-running = set()
-#ps = subprocess.Popen(['ps', 'aux', '|', 'grep', 'python'], stdout=subprocess.PIPE).communicate()[0]
-ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
-processes = ps.split('\n')
-
-for process in processes:
-    if 'python' in process:
-        for url in urls:
-            if url in process:
-                foundUrl = process[process.index('https://'):]
-                print foundUrl
-                running.add(foundUrl)
+while True:
+    running = set()
+    ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
+    processes = ps.split('\n')
+    
+    for process in processes:
+        if 'python' in process:
+            for url in urls:
+                if url in process:
+                    foundUrl = process[process.index('https://'):]
+                    print "RUNNING:",foundUrl
+                    running.add(foundUrl)
+    
+    notRunning = set.difference(urls,running)
+    print "\nNOT RUNNING:", notRunning
+    print "'"+url+"'"
+    for url in notRunning:
+        subprocess.Popen(['python','TwitterSpout.py', url])
+        None
+    time.sleep(delay)
