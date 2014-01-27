58f4edf10327e275aa2d5b0685c189318d60572c
James Schlitt <jslongboards@gmail.com>
2014-01-27 16:31:55 -0500

    daemon quick test

---
+++ SpoutDaemon.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+
+try:
+    fileIn = open(sys.argv[1])
+except:
+    fileIn = open('gdiAccounts')
+
+urls = set()
+delay = 1200
+
+content = fileIn.readlines()
+
+print "Loading URL list"
+
+for line in content:
+    if '.url=' in line.replace(' ',''):
+        urls.add(line.replace(' ','').split('=')[1])
+        
+print "GDI URLS:", urls
+
+running = set()
+#ps = subprocess.Popen(['ps', 'aux', '|', 'grep', 'python'], stdout=subprocess.PIPE).communicate()[0]
+ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
+processes = ps.split('\n')
+
+for process in processes:
+    if 'python' python in process:
+        for url in urls:
+            if url in process:
+                foundUrl = process[process.index('https://'),-1]
+                print foundUrl
+                running.add(foundUrl)
