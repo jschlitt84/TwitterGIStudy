e1fe3b556441f972cf7e0fb35dbcbb11c2be4c03
James Schlitt <jslongboards@gmail.com>
2014-02-01 15:15:48 -0500

    bugfix

--- GISpy.py
+++ GISpy.py
@@ -49,8 +49,8 @@ def updateGeoPickle(dictionary,fileRef):
     else:
         needsWrite = True
     
+    length2 = len(dictionary.keys())
     if needsWrite and loadedLength != 0 and (length2-length) > 25:
-        length2 = len(dictionary.keys())
         print "Updating master geoPickle,", length2-length1,"new locations added with",length2,"total in cache"
         pickleOut = openWhenReady(fileRef,"wb")
         pickle.dump(dictionary, pickleOut)
