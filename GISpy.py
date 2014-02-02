bb151e85617b1bd43ecdb8d81b79b23f5fc1fc3f
Ubuntu <ubuntu@ip-172-31-44-78.us-west-2.compute.internal>
2014-02-02 19:04:39 +0000

    multistreaming completed

--- GISpy.py
+++ GISpy.py
@@ -62,6 +62,8 @@ def updateGeoPickle(dictionary,fileRef):
 def getDelay(self,elapsed):
     """Calculates optimum stacked search delay for API rate limits"""
     secPerSearch = max(float(self.rateIncrement-elapsed)/self.rateLimit,0.05)
+    if self.multiAPI:
+	secPerSearch = secPerSearch/len(self.api.keys())
     return secPerSearch
 
 
